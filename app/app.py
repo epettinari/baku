7dc5e9e6405cb7436c6392cdccac344f66d9809f
Ezequiel Matias Rolon Pettinari <ezequielrolonpettinari@gmail.com>
2022-03-10 19:31:50 -0300

    Initial commit

---
+++ app/app.py
@@ -0,0 +1,14 @@
+from unicodedata import name
+from flask import Flask
+from pip import main
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def hello():
+    return 'Hello, World!'
+
+if __name__ == "__main__":
+    app.run()
+    
